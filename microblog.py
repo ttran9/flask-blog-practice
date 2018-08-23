7197c2eaac41897c26c2fa5c57776bf393511204
ttran9 <toddtran9@gmail.com>
2018-08-23 13:55:19 -0700

    Adding in a shorthand way to create objects while using flask shell command.

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    # register certain items passed into the dictionary in the return.
+    return {'db': db, 'User': User, 'Post': Post}
