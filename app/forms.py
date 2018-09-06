338e6f95bcbb882a6f8402f03f3b9e627212d0be
ttran9 <toddtran9@gmail.com>
2018-09-06 15:56:58 -0700

    Adding in the forms for the reset password request and reset password.

--- app/forms.py
+++ app/forms.py
@@ -50,3 +50,15 @@ class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
