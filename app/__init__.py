dce2baff52256a11e6d8dd1c4d0176372849e2dc
ttran9 <toddtran9@gmail.com>
2018-09-07 12:12:30 -0700

    Fixing error where the Bootstrap extension is not properly initialized.

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
-bootstrap = Bootstrap
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
