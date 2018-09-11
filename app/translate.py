04f02fb4bfada8c2e730b22ebef0d9914a3175b0
ttran9 <toddtran9@gmail.com>
2018-09-11 15:12:20 -0700

    defining a translate function.

---
+++ app/translate.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from flask_babel import _
+from app import app
+
+
+def translate(text, source_language, dest_language):
+    # TODO: Pass API Key in with HTTP header.
+    if 'YANDEX_API_TRANSLATOR_KEY' not in app.config or \
+            not app.config['YANDEX_API_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+    # auth = {'Ocp-Apim-Subscription-Key': app.config['YANDEX_API_TRANSLATOR_KEY']}
+    api_key = app.config['YANDEX_API_TRANSLATOR_KEY']
+    r = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?lang={}-{}&text={}'
+                     '&key={}'.format(source_language, dest_language, text, api_key))
+    if r.status_code != 200:
+        return _('Error: the translation service failed.')
+    return json.loads(r.content.decode('utf-8-sig'))
