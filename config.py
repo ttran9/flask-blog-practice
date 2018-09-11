9345bb061c724a1ab0b27f35f9c4c5a87bec850d
ttran9 <toddtran9@gmail.com>
2018-09-11 14:14:38 -0700

    Get the Yandex API KEY.

--- config.py
+++ config.py
@@ -15,3 +15,4 @@ class Config(object):
     ADMINS = ['wptran58@gmail.com']
     POSTS_PER_PAGE = 10
     LANGUAGES = ['en', 'es', 'vi']
+    YANDEX_API_TRANSLATOR_KEY = os.environ.get('YANDEX_API_KEY')
