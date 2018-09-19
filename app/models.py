82a0dded7f865c26af051e3a15ccf673516f005c
ttran9 <toddtran9@gmail.com>
2018-09-19 14:49:15 -0700

    Adding in a function to create a notification for a user.

--- app/models.py
+++ app/models.py
@@ -94,6 +94,12 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 class SearchableMixin(object):
     @classmethod
